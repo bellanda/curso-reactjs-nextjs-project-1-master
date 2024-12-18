d359f4f93c2e05244b1b2c5004493cd30a0d0ad6
bellanda <gustavocasadeibellanda@gmail.com>
2024-12-18 11:10:06 -0300

    fix versions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import
 import './styles/global-styles.css';
 
 import { Home } from './templates/Home';
 
-ReactDOM.render(
+// Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app
+root.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
